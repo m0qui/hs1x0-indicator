ca9fe516102cb82b836415734dc080801a2a3b91
moqui <github@moqui.me>
2016-10-06 21:47:57 +0200

    Code cleanup

--- hs1x0-indicator.py
+++ hs1x0-indicator.py
@@ -11,23 +11,19 @@ Copyright (c) 2016 moqui
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
 the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
-__author__ = "moqui"
-__license__ = "MIT"
-__version__ = "0.1"
-__email__ = "github@moqui.me"
-
 import os
 import signal
 import gi
@@ -36,13 +32,22 @@ gi.require_version('AppIndicator3', '0.1')
 from pyHS100.pyHS100 import SmartPlug
 from gi.repository import Gtk, GLib, AppIndicator3
 
+__author__ = "moqui"
+__license__ = "MIT"
+__version__ = "0.1"
+__email__ = "github@moqui.me"
+
 currentpath = os.path.dirname(os.path.realpath(__file__))
 
-### Config
-actorname = "TP-Link HS100" # Actor name to show in indicator
-showactorname = True # Show name in indicator (True|False)
-actorip = "192.168.0.100" # IP addresse of indicator
-### Config
+# Config
+# Actor name to show in indicator
+actorname = "TP-Link HS100"
+# Show name in indicator (True|False)
+showactorname = True
+# IP addresse of indicator
+actorip = "192.168.1.111"
+# Config
+
 
 def switch_actor_on(self, indicator, actor):
     actor.state = "ON"
@@ -69,12 +74,12 @@ def update_indicator(indicator, actor):
 
 def main():
         # Build application indicator
-        indicator = AppIndicator3.Indicator.new_with_path (
-        "hs1x0-indicator",
-        currentpath+"/error.svg",
-        AppIndicator3.IndicatorCategory.APPLICATION_STATUS,
-        currentpath
-        )
+        indicator = AppIndicator3.Indicator.new_with_path(
+            "hs1x0-indicator",
+            currentpath+"/error.svg",
+            AppIndicator3.IndicatorCategory.APPLICATION_STATUS,
+            currentpath
+            )
 
         actor = SmartPlug(actorip)
 
@@ -105,7 +110,7 @@ def main():
         indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
         indicator.set_icon(currentpath+"/error.svg")
         if showactorname:
-            indicator.set_label(actorname,actorname+" ")
+            indicator.set_label(actorname, actorname+" ")
 
         # Update all 1000ms
         GLib.timeout_add(1000, update_indicator, indicator, actor)
